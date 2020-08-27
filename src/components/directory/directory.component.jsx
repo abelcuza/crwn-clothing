e924a861b8077ee0c31bd406497af539f2d928c1
abel <abel.cuza07@gmail.com>
2020-08-27 12:52:19 -0400

    Added the directory component and its stylesheet

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './directory.styles.scss'
+import MenuItem from '../menu-item/menu-item.component.jsx'
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3
+                },
+                {
+                    title: 'women',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/women.png',
+                    id: 4,
+                    size: 'large'
+                },
+                {
+                    title: 'mens',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
+                    id: 5,
+                    size: "large"
+                },
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {this.state.sections.map(({title, imageUrl, id, size}) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Directory;
+
+
