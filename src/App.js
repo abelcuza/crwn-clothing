c346c62d23ded8f59f4b82120f354e51f75da21a
abel <abel.cuza07@gmail.com>
2020-08-27 12:53:59 -0400

    Included the HomePage component to show the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import HomePage from './pages/homepage/homepage.component.jsx'
 function App() {
   return (
     <div>
-      <p>Hello World</p>
+      <HomePage />
     </div>
   );
 }
