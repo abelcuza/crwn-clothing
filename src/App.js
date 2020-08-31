ed681624531245bdc6f0447ed4e0a52c48c70d9b
abel <abel.cuza07@gmail.com>
2020-08-31 10:04:58 -0400

    Update project to work with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx'
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+        </Switch>
     </div>
   );
 }
