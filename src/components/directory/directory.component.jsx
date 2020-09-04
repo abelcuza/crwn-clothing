8c8d7eb191a5491f8cf65f25d0f1ce278a33df57
abel <abel.cuza07@gmail.com>
2020-09-04 09:45:23 -0400

    Added directory.data to store static collections and updated dorectory.component to work with it

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,42 +1,14 @@
 import React from 'react'
 import './directory.styles.scss'
 import MenuItem from '../menu-item/menu-item.component.jsx'
+import DIRECTORY_DATA from './directory.data.js'
 
 class Directory extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl:'hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
-                },
-                {
-                    title: 'women',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/women.png',
-                    id: 4,
-                    size: 'large'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
-                    id: 5,
-                    size: "large"
-                },
-            ]
+            sections: DIRECTORY_DATA
         }
     }
 
---
+++ src/components/directory/directory.data.js
@@ -0,0 +1,32 @@
+const DIRECTORY_DATA = [
+    {
+        title: 'hats',
+        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        id: 1,
+        linkUrl:'hats'
+    },
+    {
+        title: 'jackets',
+        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        id: 2
+    },
+    {
+        title: 'sneakers',
+        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        id: 3
+    },
+    {
+        title: 'women',
+        imageUrl: 'https://i.ibb.co/GCCdy8t/women.png',
+        id: 4,
+        size: 'large'
+    },
+    {
+        title: 'mens',
+        imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
+        id: 5,
+        size: "large"
+    },
+];
+
+export default DIRECTORY_DATA;
