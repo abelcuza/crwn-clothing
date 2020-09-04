eaccc1cf3e578d5a78179d581867c4bfb41c54f4
abel <abel.cuza07@gmail.com>
2020-09-04 09:46:04 -0400

    Added the ShopPage, shop.data and its stylessheet

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {withRouter} from 'react-router-dom'
+import SHOP_DATA from './shop.data.js'
+
+class ShopPage extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render() {
+        return <div>
+            SHOP PAGE
+        </div>
+    }
+}
+
+export default ShopPage;
