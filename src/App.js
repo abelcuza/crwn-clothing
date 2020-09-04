0a0cdec18e1cc62f69eecc729dc78a28c846c149
abel <abel.cuza07@gmail.com>
2020-09-04 13:40:59 -0400

    Added the Header Component to the App.js script and modified the <a> tag css on App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component.jsx'
-import ShopPage from './pages/shop/shop.component.jsx'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+
 function App() {
   return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route exact path='/shop' component={ShopPage}/>
