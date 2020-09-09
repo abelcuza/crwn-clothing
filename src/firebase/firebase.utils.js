c54d46acedb3755cbc50a34cfc8fd37aec045f04
abel <abel.cuza07@gmail.com>
2020-09-09 13:59:48 -0400

    Firebase cofig to store new users auntenticated

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,25 @@ const config = {
     measurementId: "G-R5C84E2GYB"
 };
 
+// Firebase auth user storage on the database
+export const createUserProfileDocument = async (userAuth, aditionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({ displayName, email, createdAt, ...aditionalData })
+        }catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
