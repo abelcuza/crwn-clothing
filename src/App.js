308514e0466ff0446692b6c6326282307be97c58
abel <abel.cuza07@gmail.com>
2020-09-16 13:10:53 -0400

    Added a redirect if user is signed in and some modification to improve the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
@@ -31,7 +31,7 @@ class App extends React.Component {
             } else {
                 setCurrentUser(userAuth);
             }
-        });
+        })
     }
 
     componentWillUnmount() {
@@ -45,14 +45,24 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
-                    <Route exact path='/signin' component={SignInAndSignOut}/>
+                    <Route exact path='/signin' render={() =>
+                        this.props.currentUser
+                            ?
+                            (<Redirect to='/'/>)
+                            :
+                            (<SignInAndSignOut/>)
+                    }/>
                 </Switch>
             </div>)
     };
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
